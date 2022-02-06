ce4b17ef3c55f7740db52eae9185319b613f6c0f
Alessandro Barzanti <a.barzanti@gmail.com>
2022-02-06 10:26:42 +0000

    Check features on lambda

--- api/src/lambdas/job.ts
+++ api/src/lambdas/job.ts
@@ -1,5 +1,9 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { SecretsManager } from 'aws-sdk';
+import container from '../container/IOC.config';
+import BaseConfig from '../backends/configuration/abstractions/BaseConfig';
+import TYPES from '../container/IOC.types';
+import { add, format } from 'date-fns';
 
 // Create a Secrets Manager client
 const client = new SecretsManager({
@@ -24,8 +28,16 @@ export const lambdaHandler = async (
 ): Promise<APIGatewayProxyResult> => {
     const ev = JSON.stringify(event);
     const s = await getSecret();
+    const config = container.get<BaseConfig>(TYPES.BackendConfig);
+    const date = format(
+        add(new Date(), {
+            days: 30,
+        }),
+        'MM-dd-yy',
+    );
+
     return {
         statusCode: 200,
-        body: `Ev: ${ev}, Secrets: ${s}`,
+        body: `Ev: ${ev}, configEntry: ${config.getEnvironment()} , Date: ${date}, Secrets: ${s}`,
     };
 };
