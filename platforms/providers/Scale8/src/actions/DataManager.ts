236a4e682341b2726b0e1ad632057c50085e0359
Christopher Beck <cwbeck@gmail.com>
2022-04-08 22:20:18 +0100

    Added Data Manager back in..

---
+++ platforms/providers/Scale8/src/actions/DataManager.ts
@@ -0,0 +1,20 @@
+import { InputType } from '../../../../../common/enums/InputType';
+import { PlatformActionData } from '../../../../common/types/Types';
+import Logger from '../../../../common/lib/util/Logger';
+
+export const dataManager = {
+    persistence_id: 's8-data-manager',
+    name: 'Data Manager',
+    description: 'Send to data manager',
+    data: [
+        {
+            key: 'payload',
+            input_type: InputType.INGEST_ENDPOINT_PAYLOAD_DESIGNER,
+            description: 'Lorum ipsum',
+            optional: false,
+        },
+    ],
+    run: (data: PlatformActionData, log: (msg: string) => void): void => {
+        Logger.info(data.props);
+    },
+};
