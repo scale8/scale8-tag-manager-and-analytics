080c132ba25aac5e8ed4d2c09a14348e3395e222
Alessandro Barzanti <a.barzanti@gmail.com>
2022-06-26 16:19:55 +0100

    Added generated files

---
+++ ui/src/gql/generated/AlignSubscription.ts
@@ -0,0 +1,109 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+import { AlignSubscriptionInput } from "./globalTypes";
+
+// ====================================================
+// GraphQL mutation operation: AlignSubscription
+// ====================================================
+
+export interface AlignSubscription_alignSubscription_tag_manager_account {
+  __typename: "TagManagerAccount";
+  id: string;
+  /**
+   * The amount of days until the trial expires
+   */
+  trial_expires_in: number;
+  /**
+   * If the free trial is expired
+   */
+  trial_expired: boolean;
+  /**
+   * The current product id associated with this account. If this is free plan or managed, this will not be provided
+   */
+  stripe_product_id: string | null;
+  /**
+   * If the account is in a trial period
+   */
+  is_trial: boolean;
+  /**
+   * If the account is enabled
+   */
+  enabled: boolean;
+}
+
+export interface AlignSubscription_alignSubscription_data_manager_account {
+  __typename: "DataManagerAccount";
+  /**
+   * `DataManagerAccount` ID
+   */
+  id: string;
+  /**
+   * The amount of days until the trial expires
+   */
+  trial_expires_in: number;
+  /**
+   * If the free trial is expired
+   */
+  trial_expired: boolean;
+  /**
+   * The current product id associated with this account. If this is free plan or managed, this will not be provided
+   */
+  stripe_product_id: string | null;
+  /**
+   * If the account is in a trial period
+   */
+  is_trial: boolean;
+  /**
+   * If the account is enabled
+   */
+  enabled: boolean;
+}
+
+export interface AlignSubscription_alignSubscription {
+  __typename: "Org";
+  /**
+   * A unique `Org` ID
+   */
+  id: string;
+  /**
+   * Name used to describe the `Org`
+   */
+  name: string;
+  /**
+   * If this org is subscribed to any of the Scale8 services
+   */
+  is_paid: boolean;
+  /**
+   * If this org has a stripeCustomerId and can use the costumer portal
+   */
+  has_billing: boolean;
+  /**
+   * If the org is under manual invoicing
+   */
+  manual_invoicing: boolean;
+  /**
+   * A `TagManagerAccount` associated with this `Org`. A Scale8 Tag Manager account
+   * might not exist yet unless a trial has been requested or product has been subscribed to.
+   */
+  tag_manager_account: AlignSubscription_alignSubscription_tag_manager_account;
+  /**
+   * A `DataManagerAccount` associated with this `Org`. A Scale8 Data Manager
+   * account might not exist yet unless a trial has been requested or product has
+   * been subscribed to.
+   */
+  data_manager_account: AlignSubscription_alignSubscription_data_manager_account;
+}
+
+export interface AlignSubscription {
+  /**
+   * @bound=Org
+   * Aligns the accounts and org details to the payment method subscription.
+   */
+  alignSubscription: AlignSubscription_alignSubscription;
+}
+
+export interface AlignSubscriptionVariables {
+  alignSubscriptionInput?: AlignSubscriptionInput | null;
+}
